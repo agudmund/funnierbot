8fd340a0541e3c45a3a6ef148614e660d0975d1f
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2017-08-04 11:27:40 +0200

    Cleaner information about self

--- funnierbot.py
+++ funnierbot.py
@@ -14,13 +14,14 @@ funny = 'https://api.telegram.org/bot333486385:AAFGYg14V7hJJqRKyTx5tF2sb2beEu07M
 def info():
     '''Returns basic information about self'''
 
-    rez = r.get('%s/getme'%funny)
-    z=json.loads(rez.text)['result']
+    rez = r.get( '%s/getme' % funny )
+    z = json.loads(rez.text)['result']
     
-    print('My name is: %s' % z['username'])
-    print('\tbut you can call me %s....' % z['first_name'])
-    print('if you then want to be really beurocratic about it, my ID is %s' % z['id'])
+    print( 'My name is: %s' % z['username'])
+    print( '\tbut you can call me %s....' % z['first_name'])
+    print( 'if you then want to be really beurocratic about it, my ID is %s' % z['id'])
 
     return True
 
-info()
+if __name__ == '__main__':
+    info()
