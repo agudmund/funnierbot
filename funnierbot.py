60d6bb8e182beec3cdfbd6b4ae90bfb9b2e71784
Ævar Guðmundsson <aevar.gudmundsson@gmail.com>
2017-09-20 18:08:41 +0000

    basic math examples
    it asks on demand, no scoring system or incentive yet

--- funnierbot.py
+++ funnierbot.py
@@ -53,6 +53,11 @@ class Iconic:
         self.id = 'https://api.telegram.org/bot333486385:AAFGYg14V7hJJqRKyTx5tF2sb2beEu07MCg'
         self.chat_id = '339387792'
 
+        # Math Quiz things
+        self.x = 0
+        self.y = 0
+        self.z = 0
+
     def getChatID(self):
         try:
             chat_id = self.getUpdates()[-1]['message']['chat']['id']
@@ -69,6 +74,17 @@ class Iconic:
         z = json.loads( rez.text )['result']
 
         return z
+
+    def mathQuiz(self):
+        "Simple addition test"
+
+        self.x = random.randint(0,9)
+        self.y = random.randint(0,9)
+        self.z = self.x + self.y
+
+        this = "What do you get if you add %s and %s?" % (self.x,self.y)
+
+        r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,str(this),iconic.getChatID()))
 
     def wiki(self, text):
         '''Checks wikipedia for generic descriptions'''
@@ -156,6 +172,27 @@ def latest():
 
     return text
 
+def smallTalk(text):
+    if text.startswith('what is'):
+        rez = iconic.wiki(text)
+        this = urllib.parse.quote_plus(rez)
+        r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,str(this),iconic.getChatID()))
+        return
+    else:
+        r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,reply(),iconic.getChatID()))
+        return
+
+def math():
+    this = "ok, lets play a game, I'll give you numbers you tell me the answer."
+    r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,str(this),iconic.getChatID()))
+    sleep(1)
+    this = "if you want to stop the game and return to small talk just tell me 'stop' and I will stop"    
+    r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,str(this),iconic.getChatID()))
+
+    sleep(1)
+    iconic.mathQuiz()
+
+
 def listen():
     last = None
     while True:
@@ -166,21 +203,32 @@ def listen():
             if last != None:
                 text = text.lower()
 
-                if text.startswith('what is'):
-                    rez = iconic.wiki(text)
-                    this = urllib.parse.quote_plus(rez)
-                    r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,str(this),iconic.getChatID()))
-                    last = None
-                    continue
-                else:
-                    r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,reply(),iconic.getChatID()))
+                if text.lower() == 'play math':
+                    math()
                     last = None
                     continue
 
-                if text.startswith('send me audio'):
-                    sendAudio()
+                if text.lower() == 'stop':
+                    smallTalk(text)
                     last = None
                     continue
+
+                if text[0].isdigit():
+                    if text == str(iconic.z):
+                        this = 'perhaps'
+                        r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,str(this),iconic.getChatID()))
+                        sleep(3)
+                        iconic.mathQuiz()
+                    else:
+                        this = 'not quite, try again'
+                        r.post(r'%s/sendMessage?text=%s&chat_id=%s'%(funny,str(this),iconic.getChatID()))
+                    last = None
+                    continue
+
+            
+                smallTalk(text)
+                last = None
+                continue
 
             last = text
 
@@ -210,6 +258,7 @@ if __name__ == '__main__':
     parser.add_argument('--audio', help='Responds with an audio file',action="store_true")
     parser.add_argument('--image', help='Responds with an image file',action="store_true")
     parser.add_argument('--loop', help='loop command',action="store_true")
+
     args = parser.parse_args()
 
     if args.new:
@@ -225,11 +274,9 @@ if __name__ == '__main__':
         listen()
     if args.audio:
         sendAudio()
+
     if args.image:
         if args.loop:
             sendImage(chat_id=iconic.getChatID(),loop=True)
         else:
             sendImage(chat_id=iconic.getChatID())
-
-        
-    
